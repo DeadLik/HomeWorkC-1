e7975fb658ee73e6f3b1647e6eeb1c6bd0b0b033
Игорь <mc_grove@bk.ru>
2022-11-11 15:09:29 -0500

    add Task-1

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число №1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b) Console.WriteLine($"Число {a} максимальное");
+else if (a == b) Console.WriteLine("Числа равны");
+else Console.WriteLine($"Число {b} максимальное");
+
